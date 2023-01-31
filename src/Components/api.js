bdded61582fb62982dd45dc22c6799713385328d
yash-prabhakar-singh <511018066.yash@students.iiests.ac.in>
2023-01-31 13:08:11 +0530

    31/1/2023

--- src/Components/api.js
+++ src/Components/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 class api
 { key="8B8Y70UXd7o7D58A8rh7N829B629L9H8W9G7e7q9W8d";
 
+  url="http://localhost:8443";
+
 getDetails(domain)
 {
    return axios.get('https://api.dynadot.com/api3.json'/*,{ headers: {
@@ -11,47 +13,81 @@ getDetails(domain)
 
 fetchDetailsdyna(domains)
 {
-   return axios.post('http://localhost:8080/fetchdetailsdyna',domains)
+   return axios.post(`${this.url}/fetchdetailsdyna`,domains)
 }
+
+fetchgdv(domains)
+{
+   return axios.post(`${this.url}/getgdvs`,domains)
+}
+
+fetchDetailsgodaddy(domains)
+{
+   return axios.post(`${this.url}/bulkfetchgodaddy`,domains)
+}
+
+fetchDetailscloseoutsgd(domains)
+{
+   return axios.post(`${this.url}/bulkfetchcloseoutsgodaddy`,domains)
+}
+
 
    fetchDetailsnc(domains)
 {
-   return axios.post('http://localhost:8080/fetchdetailsnc',domains)
+   return axios.post(`${this.url}/fetchdetailsnc`,domains)
 }
 
 fetchDetailsdc(domains)
 {
-   return axios.post('http://localhost:8080/fetchdetailsdc',domains)
+   return axios.post(`${this.url}/fetchdetailsdc`,domains)
 }
 
 getcurrauctions()
 {
-   return axios.get('http://localhost:8080/getcurrauctions');
+   return axios.get(`${this.url}/getcurrauctions`);
 }
 
 getscheduledauctions()
 {
-   return axios.get('http://localhost:8080/getscheduledbids');
+   return axios.get(`${this.url}/getscheduledbids`);
+}
+getscheduledcloseouts()
+{
+   return axios.get(`${this.url}/getscheduledcloseouts`);
 }
 
+getcompletedcloseouts()
+{
+   return axios.get(`${this.url}/getcompletedcloseouts`);
+}
 getplacedbids()
 {
-   return axios.get('http://localhost:8080/getplacedbids');
+   return axios.get(`${this.url}/getplacedbids`);
 }
 
 startLive()
 {
-   return axios.get('http://localhost:8080/startlive');
+   return axios.get(`${this.url}/startlive`);
 }
 
 getLive()
 {
-   return axios.get('http://localhost:8080/detectlive');
+   return axios.get(`${this.url}/detectlive`);
+}
+
+startLivenc()
+{
+   return axios.get(`${this.url}/startlivenc`);
+}
+
+getLivenc()
+{
+   return axios.get(`${this.url}/getlivenc`);
 }
 
 getwatchlist()
 {
-   return axios.get('http://localhost:8080/getwatchlist');
+   return axios.get(`${this.url}/getwatchlist`);
 }
 
 watchlist(ids, rows)
@@ -63,58 +99,80 @@ watchlist(ids, rows)
    console.log(ids)
    console.log(nids)
    const arr=[ids,nids];
-   return axios.put('http://localhost:8080/watchlisted',arr);
+   return axios.put(`${this.url}/watchlisted`,arr);
 }
 
 
 getcompletedauctions()
 {
-   return axios.get('http://localhost:8080/getcompletedauctions');
+   return axios.get(`${this.url}/getcompletedauctions`);
 }
 
 getnotifs()
 {
-   return axios.get('http://localhost:8080/getnotifications');
+   return axios.get(`${this.url}/getnotifications`);
 }
 
 sample()
 {
-   return axios.get('http://localhost:8080/sample');
+   return axios.get(`${this.url}/sample`);
 }
 
 schedulebiddc(ddlist)
 {
-return axios.post("http://localhost:8080/bulkbidscheduledc1",ddlist);
+return axios.post(`${this.url}/bulkbidscheduledc1`,ddlist);
 }
 
 instantbiddc(ddlist)
 {
-return axios.post("http://localhost:8080/bulkbiddc",ddlist);
+return axios.post(`${this.url}/bulkbiddc`,ddlist);
+}
+
+schedulebidgd(ddlist)
+{
+return axios.post(`${this.url}/bulkbidschedulegodaddy`,ddlist);
+}
+
+instantbidgd(ddlist)
+{
+return axios.post(`${this.url}/bulkbidgodaddy`,ddlist);
+}
+
+schedulecloseoutgd(closeout, price)
+{
+   let closeouts={closeout,price}
+return axios.post(`${this.url}/schedulegodaddycloseouts`,closeouts);
+}
+
+instantcloseoutgd(closeout,price)
+{
+   let closeouts={closeout,price}
+return axios.post(`${this.url}/buygodaddycloseouts`,closeouts);
 }
 
 schedulebidnc(ddlist)
 {
-return axios.post("http://localhost:8080/bulkschedulenc",ddlist);
+return axios.post(`${this.url}/bulkschedulenc`,ddlist);
 }
 
 instantbidnc(ddlist)
 {
-return axios.post("http://localhost:8080/bulkbidnc",ddlist);
+return axios.post(`${this.url}/bulkbidnc`,ddlist);
 }
 
 instantbidd(ddlist)
 {
-return axios.post("http://localhost:8080/bulkbidd",ddlist);
+return axios.post(`${this.url}/bulkbidd`,ddlist);
 }
 
 schedulebiddyna(ddlist)
 {
-return axios.post("http://localhost:8080/postDomains",ddlist);
+return axios.post(`${this.url}/postDomains`,ddlist);
 }
 
 instantbiddyna(ddlist)
 {
-return axios.post("http://localhost:8080/postDomainsinstant",ddlist);
+return axios.post(`${this.url}/postDomainsinstant`,ddlist);
 }
 
 }
